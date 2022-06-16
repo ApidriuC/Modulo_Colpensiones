55f75989ac14345a0770754a71c2ba45d8369f20
Andrés Felipe Londoño Campos <78039311+ApidriuC@users.noreply.github.com>
2022-06-16 15:52:47 -0500

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,98 @@
+
+function pintarButton0() {
+
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button0").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton1() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button1").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton2() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button2").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton3() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button3").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton4() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button4").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton5() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button5").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton6() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button6").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button7").style.backgroundColor = "#FFFFFF";
+}
+function pintarButton7() {
+    document.getElementById("continuar").style.backgroundColor = "#1C50E3";
+    document.getElementById("continuar").style.pointerEvents = "visible"
+    document.getElementById("button7").style.backgroundColor = "#C1C1C1";
+    document.getElementById("button0").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button1").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button3").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button4").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button5").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button6").style.backgroundColor = "#FFFFFF";
+    document.getElementById("button2").style.backgroundColor = "#FFFFFF";
+} 
---
+++ assets/js/continue.js
@@ -0,0 +1,22 @@
+
+
+function openModal() {
+    document.getElementById("continueModal").style.display = "block";
+    document.getElementById("desplegarModal").style.top = "-256%";
+}
+function closeModal() {
+    document.getElementById("continueModal").style.display = "none";
+    document.getElementById("tablas").style.display = "block";
+}
+
+function continueModal() {
+    if (document.getElementById("continueModal").style.display != "none") {
+        closeModal();
+    }
+    else {
+        document.getElementById("tablas").style.display = "none";
+        document.getElementById("middlePage").style.opacity = "1";
+        openModal();
+    }
+}
+
